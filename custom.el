f3b05ff8ba1d264d134b2444c841426593e86408
David Conner <dconner.pro@gmail.com>
2021-04-14 09:24:05 -0400

    commit, though org-babel-tangle-async keybindings don't work

--- custom.el
+++ custom.el
@@ -5,6 +5,8 @@
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
    '("4bca89c1004e24981c840d3a32755bf859a6910c65b829d9441814000cf6c3d0" default))
+ '(org-agenda-files
+   '("~/.config/dc/.doom.d/config.org" "/data/org/README.org" "/data/org/clojure.org" "/data/org/devops.org" "/data/org/etc.org" "/data/org/kde.org" "/data/org/keausa.org" "/data/org/project-ideas.org" "/data/org/synthbio.org"))
  '(package-selected-packages '(pkgbuild-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
