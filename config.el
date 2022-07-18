37b0a9db08fb7ddd9d50290993857e37a268e933
David Conner <dconner.pro@gmail.com>
2022-07-18 15:17:01 -0400

    change fonts because cantarell cant build
    ... remind me again ... which of these fonts is cantarell?
    
    i don't actually know the answer

--- config.el
+++ config.el
@@ -115,8 +115,8 @@
 ;; (set-frame-font "Source Code Pro 12" nil t)
 ;; Source Code Pro not available in pGTK
 (setq doom-font (font-spec :family "DejaVu Sans Mono" :size 14)
-      doom-unicode-font (font-spec :family "DejaVu Sans Mono" :size 14)
-      doom-variable-pitch-font (font-spec :family "DejaVu Serif" :size 14)
+      doom-variable-pitch-font (font-spec :family "Overpass" :size 14)
+      doom-unicode-font (font-spec :family "JuliaMono" :size 14)
       doom-font-increment 2)
 
 ;; (unless (find-font doom-font)
