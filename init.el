eb5662ee403dab5ce5cdf15c70a7aaf53ff1abc8
David Conner <dconner.pro@gmail.com>
2022-11-17 14:56:22 -0500

    add org-lms

--- init.el
+++ init.el
@@ -98,7 +98,7 @@
        (magit +forge)
        make
        ;; pass              ; password manager for nerds
-       pdf                              pdf enhancements
+       pdf                              ;; pdf enhancements
        prodigy           ; FIXME managing external services & code builders
        rgb                              ; creating color strings
 
